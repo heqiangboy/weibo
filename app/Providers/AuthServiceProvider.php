34468b22f42fd026ac22280d3473656f7e498d94
HU <heqiangboy@126.com>
2019-04-22 03:50:12 +0000

    全新系统4

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+//        'App\Model' => 'App\Policies\ModelPolicy',
         \App\Models\User::class  => \App\Policies\UserPolicy::class,
     ];
 
@@ -25,7 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+//
+        // 修改策略自动发现的逻辑
+//        Gate::guessPolicyNamesUsing(function ($modelClass) {
+//            // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
+//            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+//        });
     }
 }

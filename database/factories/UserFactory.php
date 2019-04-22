99ec5b3abb848b82c7c1b20525a295885cc0f9cc
HU <heqiangboy@126.com>
2019-04-22 08:05:43 +0000

    用户激活

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,7 +8,8 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'activated' => true,
+        'password' => '$2y$10$qEQ42Nn4jbj9kRaV95aevO5y38m9bbHmBBPonsvzSRfQJDpGZbcA.', // secret
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,15 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
